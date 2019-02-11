6d5e12638ab9eee0e8411155bda90f28f8c6b1ac
Ram6651SKIT <33284645+Ram6651SKIT@users.noreply.github.com>
2019-02-12 01:46:29 +0530

    functionality stage 2

--- src/context.js
+++ src/context.js
@@ -10,7 +10,7 @@ const ProductContext=React.createContext();
  	state={
  		products:[],
 		 detailProduct:detailProduct,
-		 cart:storeProducts,
+		 cart:[],
 		 modalOpen:false,
 		 modalProduct:detailProduct,
 		 cartSubTotal:0,
@@ -62,7 +62,7 @@ const ProductContext=React.createContext();
 			 },
 
 			 () => {
-				 console.log(this.state);
+				 this.addTotals();
 			 }
 		 );
 	 };
@@ -81,17 +81,100 @@ const ProductContext=React.createContext();
 	 };
 	 
 increment =(id)=>{
-console.log('This is increment method');
+let tempCart =[...this.state.cart];
+const selectedProduct=tempCart.find(item =>item.id===id);
+ 
+const index=tempCart.indexOf(selectedProduct);
+const product=tempCart[index];
+
+product.count=product.count+1;
+product.total=product.count*product.price;
+
+this.setState(
+	()=>{
+		return { cart:[...tempCart]};
+	},
+	()=>{
+		this.addTotals();
+	}
+);
 }
 decrement =(id)=>{
-	console.log('This is decrement method');
+	let tempCart =[...this.state.cart];
+	const selectedProduct=tempCart.find(item =>item.id===id);
+	 
+	const index=tempCart.indexOf(selectedProduct);
+	const product=tempCart[index];
+	
+	product.count=product.count-1;
+
+	if(product.count===0){
+		this.removeItem(id);
+	}
+	else{
+		product.total=product.count*product.price;
+		this.setState(
+			()=>{
+				return { cart:[...tempCart]};
+			},
+			()=>{
+				this.addTotals();
+			}
+		);
+	}
 	}
 
-	removeItem=(id)=>{
-		console.log('Item removed');
+	removeItem= id =>{
+		let tempProducts =[...this.state.products];
+		let tempCart =[...this.state.cart];
+
+		tempCart =tempCart.filter(item=>item.id !==id);
+
+		const index=tempProducts.indexOf(this.getItem(id));
+		let removedProduct=tempProducts[index];
+		removedProduct.inCart=false;
+		removedProduct.count=0;
+		removedProduct.total=0;
+
+		this.setState(
+			()=>{
+				return {
+					cart:[...tempCart],
+					products:[...tempProducts]
+				};
+			},
+			()=>{
+				this.addTotals();
+			}
+		);
+
 	}
 	clearCart=()=>{
-		console.log('Cart was cleared');
+		this.setState(
+			()=>{
+               return {cart: [] };
+			},
+
+			()=>{
+				this.setProducts();
+				this.addTotals();
+			}
+		);
+	};
+
+	addTotals = () =>{
+		let subTotal =0;
+		this.state.cart.map(item=>(subTotal+=item.total));
+		const tempTax=subTotal*0.1;
+		const tax=parseFloat(tempTax.toFixed(2));
+		const total=subTotal+tax
+		this.setState (()=>{
+			return {
+				cartSubTotal:subTotal,
+				cartTax:tax,
+				cartTotal:total
+			}
+		})
 	}
 
 	render(){
