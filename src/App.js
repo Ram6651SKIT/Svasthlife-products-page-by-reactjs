6681fcc41d6375a257e2ca3f89f92ad2047adf58
unknown <rammohan6651@gmail.com>
2019-09-23 16:47:34 +0530

    added sass styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import{Switch,Route} from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar';
 import Cart from './components/Cart';
