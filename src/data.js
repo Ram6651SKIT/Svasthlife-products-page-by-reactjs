cc1649e7a6af4058c2f15906bc5e81d171499deb
Ram6651SKIT <33284645+Ram6651SKIT@users.noreply.github.com>
2019-09-24 23:44:47 +0530

    product description

--- src/data.js
+++ src/data.js
@@ -44,7 +44,7 @@ export const storeProducts = [
     price: 405,
     company: "Svasthlife",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Svasth Life pure cow ghee is made by the traditional hand churning method. We maintain the culture and standards of making ghee without any chemical and preservatives. Svasth Life ghee assures good health as we preserve the medicinal properties of it by making it in a natural way. Get 400 G and 800 G at Rs. 590 & Rs. 1180 Respectively.",
     inCart: false,
     count: 0,
     total: 0
