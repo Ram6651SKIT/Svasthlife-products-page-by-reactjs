6375287bc82bf864bd3f4b805092203dccbd5b81
Ram6651SKIT <33284645+Ram6651SKIT@users.noreply.github.com>
2019-09-24 23:50:18 +0530

    testing

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -23,7 +23,7 @@ export default class Modal extends Component {
                           <div className="row">
                           <div  id='modal' className="col-8 mx-auto col-md-6 col-lg-4 
                           text-center text-capitalize p-5">
-                          <h5>Item added to the cart</h5>
+                          <h5>product added to the cart</h5>
                           <img src={img} className="img-fluid" alt="product" />
                           <h5>{title}</h5>
                           <h5 className="text-muted">Price: &#8377;{price}</h5>
